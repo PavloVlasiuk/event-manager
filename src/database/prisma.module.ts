be72dbe6b0f0eba9d563d0d21035af5cff960eac
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-11-21 02:06:12 +0200

    Implemented refresh token repository

--- src/database/prisma.module.ts
+++ src/database/prisma.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from './repositories/user.repository';
+import { RefreshTokenRepository } from './repositories/refresh-token.repository';
 
 @Global()
 @Module({
-  providers: [PrismaService, UserRepository],
-  exports: [PrismaService, UserRepository],
+  providers: [PrismaService, UserRepository, RefreshTokenRepository],
+  exports: [PrismaService, UserRepository, RefreshTokenRepository],
 })
 export class PrismaModule {}
