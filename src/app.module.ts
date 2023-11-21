a920cf15d4e614f0b42e58a8d2127c73761172f7
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-11-21 02:00:23 +0200

    Created AppConfigModule and SecurityConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { AppConfigModule } from './modules/config/app-config-module.module';
+import * as process from 'process';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    AppConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.${process.env.NODE_ENV}.env`],
+    }),
+  ],
 })
 export class AppModule {}
