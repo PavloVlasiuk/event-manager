e2112a7076515c78f700148f6eb76bf27ac9369b
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-01-12 01:29:45 +0200

    Implemented email sending logic with mailer: - created EmailService with sendTemplatedEmail method - created EmailModule - created template for email message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { AppConfigModule } from './modules/config/app-config.module';
 import * as process from 'process';
+import { EmailModule } from './modules/email/email.module';
 
 @Module({
   imports: [
     AuthModule,
+    EmailModule,
     AppConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.${process.env.NODE_ENV}.env`],
